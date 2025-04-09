d248eb22ac1022488ad29fed43b29319c3dd38bf
remiRenard <rem.renard36@gmail.com>
2025-04-09 20:03:00 +0200

    fix gitignore .env

--- app/index.php
+++ app/index.php
@@ -1,24 +1,5 @@
 <?php
 
-// use Dotenv\Dotenv;
-
-// $dotenv = Dotenv::createImmutable(dirname(__DIR__));
-
-// $dotenv->load();
-
-// $servername = getenv('DB_HOST');
-// $username = getenv('DB_USER');
-// $password = getenv('DB_PASSWORD');
-// $dbname = getenv('DB_NAME');
-// $port = getenv('DB_PORT');
-
-// $conn = new mysqli($servername, $username, $password, $dbname, $port);
-
-// if ($conn->connect_error) {
-//     die("Connexion échouée : " . $conn->connect_error);
-// }
-// "Connexion réussie à la base de données !";
-
 use App\Router;
 use App\Controllers\{User, Auth};
 
